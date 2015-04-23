b3c6c423b31f5cd74f726b4bb5106c87ebd561a1
Veldhoen <sara.veldhoen+github@gmail.com>
2015-04-23 16:44:05 +0200

    IORNN with checked backprop

--- IORNN.py
+++ IORNN.py
@@ -5,118 +5,137 @@ import numpy as np
 import sys
 from collections import defaultdict
 import random
+import copy
 
 class Node:
-  def __init__(self, children, cat, nonlinearity):
+  def __init__(self, children, cat, actI,actO):
     self.cat = cat
-    self.nonlin = nonlinearity
+    self.actI = actI
+    self.actO = actO
     self.children = children
+    if len(children)==1:
+      self.children[0].setRelatives(self,None)
+    if len(children)==2:
+      self.children[0].setRelatives(self,self.children[1])
+      self.children[1].setRelatives(self,self.children[0])
+    if len(children)>2:
+      print 'Something is rotten'
     self.parent = None
-    self.singling = None
+    self.sibling = None
+  def recomputeNW(self, theta):
+    try: self.parent.recomputeNW(theta)
+    except:
+      self.inner(theta)
+      self.outer(theta)
+
 
   def setRelatives(self, parent, sibling):
+#    print 'self: [', self,'] (', self.cat,  '). Parent: [',parent, ']. Sibling: [', sibling,']'
     self.parent = parent
     self.sibling = sibling
 
-  def forwardOuter(self,theta):
-    [child.forwardOuter(theta) for child in self.children]
-
   def backpropInner(self,delta,theta,gradients):
+#    print 'backpropInner', self
     childrenas = np.concatenate([child.innerA for child in self.children])
     childrenads = np.concatenate([child.innerAd for child in self.children])
     # compute delta to backprop and backprop it
-    deltaB = np.split(np.multiply(np.transpose(theta[self.cat+'MI']).dot(delta),childrenads),len(self.children))
+    deltaB = np.split(np.multiply(np.transpose(theta[self.cat+'IM']).dot(delta),childrenads),len(self.children))
     [self.children[i].backpropInner(deltaB[i], theta, gradients) for i in range(len(self.children))]
     # update gradients for this node
-    gradients[self.cat+'MI']+= np.outer(delta,childrenas)
-    gradients[self.cat+'BI']+=delta
+    gradients[self.cat+'IM']+= np.outer(delta,childrenas)
+    gradients[self.cat+'IB']+=delta
 
   def backpropOuter(self, delta, theta, gradients):
+#    print 'backpropOuter', self#, self.sibling
     if self.parent:
-      As = np.concatenate([self.parent.outerA,self.sibling.innerA])
-      Ads = np.concatenate([self.parent.outerAd,self.sibling.innerAd])
+      As = self.parent.outerA
+      Ads = self.parent.outerAd
+      if self.sibling:
+        As = np.append(As,self.sibling.innerA)
+        Ads = np.append(Ads,self.sibling.innerAd)
       cat = self.parent.cat
-      M = theta[cat+'MO']
-      deltaB = np.split(np.multiply(np.transpose(M).dot(delta), Ads),2)
-      self.parent.backpropOuter(deltaB[0], theta, gradients)
-      self.sibling.backpropInner(deltaB[1], theta, gradients)
-      gradients[cat+'MO']+= np.outer(delta,As)
-      gradients[cat+'BO']+= delta
+      M = theta[cat+'OM']
+#      print cat, np.shape(M), np.shape(delta)
+      deltaB = np.multiply(np.transpose(M).dot(delta), Ads)
+      if self.sibling:
+#        print 'backproping to parent and sibling'
+        deltaB = np.split(deltaB,2)
+        self.parent.backpropOuter(deltaB[0], theta, gradients)
+        self.sibling.backpropInner(deltaB[1], theta, gradients)
+      else: self.parent.backpropOuter(deltaB, theta, gradients)
+      gradients[cat+'OM']+= np.outer(delta,As)
+      gradients[cat+'OB']+= delta
 
   def inner(self, theta):
     inputsignal = np.concatenate([child.inner(theta) for child in self.children])
-    M= theta[self.cat+'MI']
-    b= theta[self.cat+'BI']
+    M= theta[self.cat+'IM']
+    b= theta[self.cat+'IB']
     self.innerZ = M.dot(inputsignal)+b
-    self.innerA, self.innerAd = activate(self.innerZ,self.nonlin)
+    self.innerA, self.innerAd = activate(self.innerZ,self.actI)
     return self.innerA
 
   def outer(self, theta):
-    if self.parent:
-      inputsignal = np.concatenate([self.parent.outer(theta),self.sibling.inner(theta)])
-      cat = self.parent.cat
-      M= theta[cat+'MO']
-      b= theta[cat+'BO']
-      self.outerZ = M.dot(inputsignal)+b
+#    print 'outer called for:', self,  'of cat', self.cat
+    if not self.parent:
+      self.outerZ = np.zeros_like(theta[self.cat+'IB'])
     else:
-      self.outerZ = np.zeros(25)
-    self.outerA, self.outerAd = activate(self.outerZ,self.nonlin)
-    return self.outerA
+      if self.sibling: inputsignal = np.concatenate([self.parent.outerA,self.sibling.innerA])
+      else: inputsignal = self.parent.outerA
+      cat = self.parent.cat
+      M= theta[cat+'OM']
+      b= theta[cat+'OB']
+      self.outerZ = M.dot(inputsignal)+b
+    self.outerA, self.outerAd = activate(self.outerZ,self.actO)
+    [child.outer(theta) for child in self.children]
+#    return self.outerA
 
   def __str__(self):
     if self.cat == 'comparison': return '['+'] VS ['.join([str(child) for child in self.children])+']'
     elif self.cat == 'softmax': return ''.join([str(child) for child in self.children])
+    elif self.cat == 'u': return '<'+' '.join([str(child) for child in self.children])+'>'
+    elif self.cat == 'score': return 's'
     else: return '('+' '.join([str(child) for child in self.children])+')'
 
 class Leaf(Node):
-  def __init__(self, cat, index, word=''):
-    Node.__init__(self,[],cat,'identity')
+  def __init__(self, cat, index, actO, word=''):
+    children = [Node([Node([], 'score', 'identity','identity')], 'u', 'tanh','tanh')]
+    Node.__init__(self,children,cat,'identity',actO)
+    children[0].setRelatives(self,self)
+    # this node should behave as a sibling (produce an inner representation) and a parent (outer) to the u node
     self.index = index
     self.word = word
 
-  def forwardOuter(self,theta):
-    self.outer(theta)
-
-  def backpropOuter(self, theta, gradients):
-    delta =
-    Node.backpropOuter(self, delta, theta, gradients)
 
 
-      cat = self.parent.cat
-      M = theta[cat+'MO']
-      deltaB = np.split(np.multiply(np.transpose(M).dot(delta), Ads),2)
-      self.parent.backpropOuter(deltaB[0], theta, gradients)
-      self.sibling.backpropInner(deltaB[1], theta, gradients)
-      gradients[cat+'MO']+= np.outer(delta,As)
-      gradients[cat+'BO']+= delta
+  def score(self, theta, wordIndex=-1, recompute = True):
+    self.recomputeNW(theta)
+    # pretend the index is the candidate
+    if wordIndex > 0:
+      trueIndex = self.index
+      self.index = wordIndex
+      self.inner(theta)
+      self.outer(theta)
+    score = self.children[0].children[0].outerA
+    # reset everything
+    if wordIndex > 0:
+      self.index = trueIndex
+      self.inner(theta)
+      self.outer(theta)
+    return score[0]
 
   def inner(self, theta):
-    self.innerZ = theta[self.cat][self.index]
-    self.innerA, self.innerAd = activate(self.innerZ,self.nonlin)
+
+    self.innerZ = theta[self.cat+'IM'][self.index]
+    self.innerA, self.innerAd = activate(self.innerZ,self.actI)
     return self.innerA
 
 
   def backpropInner(self,delta, theta, gradients):
-    gradients[self.cat][self.index] += delta
+#    print 'backpropInner', self
+    gradients[self.cat+'IM'][self.index] += delta
 
   def __str__(self):
     return self.word
-
-  def score(self, theta, wordIndex=-1):
-    if wordIndex <0: wordIndex = self.index
-    M = theta['uM']
-    b = theta['uB']
-    some = M.dot(np.concatenate([self.outerA,theta['word'][wordIndex]]))+b
-#    some = M[0].dot(self.outerA) + M[1].dot(theta['word'][wordIndex]) + b
-    u ,ud = activate(some, self.nonlin)
-    score = activate(np.transpose(theta['scoreM']).dot(u),'tanh')
-    return score
-
-# class Top(Node):
-#   def __init__(self, children, cat, nonlinearity):
-#     Node.__init__(self, children, cat, nonlinearity)
-#     self.parent = None
-#     self.sibling = None
 
 def activate(vector, nonlinearity):
   if nonlinearity =='identity':
@@ -124,7 +143,7 @@ def activate(vector, nonlinearity):
     der = np.ones_like(act)
   elif nonlinearity =='tanh':
     act = np.tanh(vector)
-    der = 1- np.multiply(act,act)
+    der = 1- np.square(act)
   elif nonlinearity =='ReLU':
     act = np.array([max(x,0)+0.01*min(x,0) for x in vector])
     der = np.array([1*(x>=0) for x in vector])
@@ -141,7 +160,8 @@ def activate(vector, nonlinearity):
     der = np.ones(len(act))
   return act, der
 
-def numericalGradient(theta, nw, w):
+def numericalGradient(theta, nw):
+#  print 'numgrad', theta.dtype.names
   epsilon = 0.0001
   numgrad = np.zeros_like(theta)
   score0 = nw.score(theta)
@@ -152,9 +172,9 @@ def numericalGradient(theta, nw, w):
         i = it.multi_index
         old = theta[name][i]
         theta[name][i] = old + epsilon
-        errorPlus = max(0,1-score0+nw.score(theta, w))
+        errorPlus = max(0,1-score0+nw.score(theta))
         theta[name][i] = old - epsilon
-        errorMin = max(0,1-score0+nw.score(theta, w))
+        errorMin = max(0,1-score0+nw.score(theta))
         d =(errorPlus-errorMin)/(2*epsilon)
         numgrad[name][i] = d
         theta[name][i] = old  # restore theta
@@ -170,25 +190,43 @@ def compareGrad(numgrad,grad):
     if np.array_equal(gr,ngr): diff = 0
     else: diff = np.linalg.norm(ngr-gr)/(np.linalg.norm(ngr)+np.linalg.norm(gr))
     print 'Difference '+name+' :', diff
+#     if True:
+# #    if name == 'uMO' :
+#       for i in range(len(ngr)):
+#         print ngr[i],gr[i]
     gradflat = np.append(gradflat,gr)
     numgradflat = np.append(numgradflat,ngr)
   print 'Difference overall:', np.linalg.norm(numgradflat-gradflat)/(np.linalg.norm(numgradflat)+np.linalg.norm(gradflat))
 
-def trainPredict(nw, theta,vocabulary,gradients=None):
-  if gradients==None: gradients = np.zeros_like(theta)
-  if isinstance(nw,Leaf):
-    score0 = nw.score(theta)
-    for n in range(5):
-      # create candidate index unlike the observed one
-      i = random.randint(0,len(vocabulary)-1)
-      while i == nw.index:
-        i = random.randint(0,len(vocabulary)-1)
-      # if the candidate scores too high: backpropagate error
-      score1 = nw.score(theta, i)
-      if 1-score0+score1>0:
-        delta = np.reshape(np.split(theta['uM'],2)[0].dot(theta['scoreM']),-1)
+def trainPredict(nw, theta,vocabulary,gradients):
+  print 'training:', nw
 
-        numGrad = numericalGradient(theta, nw, i)
-        nw.backpropOuter(delta, theta, gradients)
-        compareGrad(numGrad,gradients)
-  else: [trainPredict(child, theta, vocabulary, gradients) for child in nw.children]
+  if isinstance(nw,Leaf):
+
+    scorew = nw.score(theta, False)
+    for n in range(3):
+      anGrad = np.zeros_like(theta)
+      # create candidate index unlike the observed one
+      x = random.randint(0,len(vocabulary)-1)
+      while x == nw.index:
+        x = random.randint(0,len(vocabulary)-1)
+
+      # if the candidate scores too high: backpropagate error
+      scorex = nw.score(theta, x, False)
+      c = 1 - scorew+scorex
+      print 'random guess', n, 'c:',c
+      if c>1: # scorew<scorex
+        numGrad = numericalGradient(theta, nw)
+        delta = np.array([1])
+        nw.children[0].children[0].backpropOuter(delta, theta, anGrad)
+        compareGrad(numGrad,anGrad)
+
+    # update (or instantiate) variable gradients
+#    if gradients is None:
+#      print 'instantiated gradient.'
+#      gradients = anGrad
+#    else:
+    for name in gradients.dtype.names:
+      gradients[name] += anGrad[name]
+  [trainPredict(child, theta, vocabulary, gradients) for child in nw.children]
+#  return gradients
--- natlogIO.py
+++ natlogIO.py
@@ -6,20 +6,20 @@ from collections import defaultdict
 import numpy as np
 import random
 
-from IORNN import *
+from IORNN3 import *
 from training import *
 from getEmbeddings import *
 
 from params import *
 
-def iornnFromTree(tree, vocabulary, wordReduction = False, grammarBased = False):
+def iornnFromTree(tree, vocabulary, grammarBased = False):
   if tree.height() > 2:
     if grammarBased: cat = tree.label()+' -> '+' '.join([child.label() for child in tree])
     else: cat = 'composition'
-    children = [iornnFromTree(child,vocabulary,wordReduction) for child in tree]
-    parent = Node(children,cat,'tanh')
-    children[0].setRelatives(parent,children[1])
-    children[1].setRelatives(parent,children[0])
+    children = [iornnFromTree(child,vocabulary, grammarBased) for child in tree]
+    parent = Node(children,cat,'tanh','tanh')
+#    children[0].setRelatives(parent,children[1])
+#    children[1].setRelatives(parent,children[0])
     return parent
   else: #preterminal node
     words = tree.leaves()
@@ -27,64 +27,66 @@ def iornnFromTree(tree, vocabulary, wordReduction = False, grammarBased = False)
     else: 'Not exactly one leaf?!', tree
     try: index = vocabulary.index(word)
     except: index = 0
-    leaf = Leaf('word',index, word)
-
-    if wordReduction:
-    # wordReduction adds an extra layer to reduce high-dimensional words
-    # to the dimensionality of the inner representations
-      if grammarBased: cat = tree.label()
-      else: cat = 'preterminal'
-      parent = Node([leaf],cat,'tanh')
-      leaf.setRelatives(parent,None)
-      return parent
-    else: return leaf
+    leaf = Leaf('word',index, 'tanh',word)
+    return leaf
 
 def initialize(dwords, dint, dcomp, nrel, nwords = 1, V = None):
   # initialize all parameters randomly using a uniform distribution over [-0.1,0.1]
   types = []
 #  types.append(('preterminalM','float64',(dint,dwords)))
 #  types.append(('preterminalB','float64',(dint)))
-  types.append(('compositionMI','float64',(dint,2*dint)))
-  types.append(('compositionBI','float64',(dint)))
-  types.append(('compositionMO','float64',(dint,2*dint)))
-  types.append(('compositionBO','float64',(dint)))
+  types.append(('compositionIM','float64',(dint,2*dint)))
+  types.append(('compositionIB','float64',(dint)))
+  types.append(('compositionOM','float64',(dint,2*dint)))
+  types.append(('compositionOB','float64',(dint)))
 #  types.append(('comparisonM','float64',(dcomp,2*dint)))
 #  types.append(('comparisonB','float64',(dcomp)))
 #  types.append(('classifyM','float64',(nrel,dcomp)))
 #  types.append(('classifyB','float64',(nrel)))
-  types.append(('uM', 'float64',(2*dint,2*dint)))
-  types.append(('uB', 'float64',(2*dint)))
-  types.append(('scoreM', 'float64',(2*dint,1)))
-  types.append(('word','float64',(nwords,dwords)))
+  types.append(('wordIM','float64',(nwords,dwords)))
+  types.append(('wordOM', 'float64',(2*dint,2*dint)))
+  types.append(('wordOB', 'float64',(2*dint)))
+  types.append(('uOM', 'float64',(1,2*dint)))
+#  types.append(('uBO', 'float64',(1)))
+  types.append(('uOB', 'float64',(1,1))) #matrix with one value, a 1-D array with only one value is a float and that's problematic with indexing
+
   theta = np.zeros(1,dtype = types)
   for name, t, size in types:
     if isinstance(size, (int,long)): theta[name] = np.random.rand(size)*.02-.01
     elif len(size) == 2: theta[name] = np.random.rand(size[0],size[1])*.02-.01
     else: print 'invalid size:', size
   if 0 in theta: print 'zero in theta!'
+  print 'created Theta:', theta.dtype.names
   return theta[0]
 
 relations = ['<','>','=','|','^','v','#']
-vocabulary = ['UNK', 'all','no','warthogs','walk','talk']
-dwords = 25
-dint = 25
+vocabulary = ['UNK', 'all','no','warthogs','hippos','dogs','bark','walk','talk']
+dwords = 5
+dint = 5
 dcomp = 75
 nrel = len(relations)
 nwords = len(vocabulary)
 theta = initialize(dwords, dint, dcomp, nrel, nwords)
+#s1 = '( all warthogs ) warthogs'
 s1 = '( all warthogs ) walk'
 t1 = nltk.tree.Tree.fromstring('('+re.sub(r"([^()\s]+)", r"(W \1)", s1)+')')
 nw1 = iornnFromTree(t1, vocabulary)
-print nw1.inner(theta)
-print nw1.outer(theta)
+#print nw1.inner(theta)
+#print nw1.outer(theta)
 
-def printReps(nw,theta):
-  print nw, 'Inner:',nw.inner(theta), 'Outer:',nw.outer(theta)
-  for child in nw.children: printReps(child,theta)
+#def printReps(nw,theta):
+#  print nw, 'Inner:',nw.inner(theta), 'Outer:',nw.outer(theta)
+#  for child in nw.children: printReps(child,theta)
 
 #printReps(nw1,theta)
-nw1.forwardOuter(theta)
-trainPredict(nw1, theta,vocabulary)
+#print theta.dtype
+nw1.inner(theta)
+nw1.outer(theta)
+gradients = np.zeros_like(theta)             
+trainPredict(nw1, theta,vocabulary, gradients)
+# for name in gradients.dtype.names:
+#   theta[name] = theta[name] + gradients[name]
+
 #line = '=	 ( all warthogs ) walk	 ( all warthogs ) walk'
 #relation, s1, s2 = bits = line.split('\t')
 #trees = [nltk.tree.Tree.fromstring('('+re.sub(r"([^()\s]+)", r"(W \1)", s)+')') for s in [s1,s2]],relations.index(relation)
