f53b9997356f3cc624d41c50e87f9e5d5c3cc78b
Veldhoen <sara.veldhoen+github@gmail.com>
2015-05-04 15:37:18 +0200

    generalized datahandling, another new version of data, removed originals, added flickr data for unsupervised training

--- getData.py
+++ getData.py
@@ -2,10 +2,11 @@ import pickle, os
 import nltk, re
 import numpy as np
 import random
+import sys
 
 def getSennaEmbs(source, voc = ['UNK']):
   print '\tObtaining embeddings...'
-  source = os.path.join('data',name)
+  source = os.path.join('../originalData',name)
   getVoc = (voc == ['UNK'])
   L = [0]*len(voc)
   with open(source+'/words.lst','r') as words:
@@ -26,86 +27,59 @@ def getSennaEmbs(source, voc = ['UNK']):
   with open(os.path.join('data',name+'.pik'), 'wb') as f:
     pickle.dump([V,voc], f, -1)
 
-
-
-def artData(name):
-  source = os.path.join('data',name)
+def storeTrees(name):
+  source = os.path.join('../originalData',name)
   # make a list of files to open
   if os.path.isdir(source):
     toOpen = [os.path.join(source,f) for f in os.listdir(source)]
     toOpen = [f for f in toOpen if os.path.isfile(f)]
   elif os.path.isfile(source): toOpen = [source]
-
-
-  examples = []
-  vocabulary = ['UNK']
-  for f in toOpen:
-    with open(f,'r') as f:
-      for line in f:
-        bits = line.split('\t')
-        if len(bits) == 3:
-          relation, s1, s2 = bits
-          # add unknown words to vocabulary
-          for word in s1.split()+s2.split():
-            if word !=')' and word != '(' and word not in vocabulary:
-              vocabulary.append(word)
-          # add training example to set
-
-          examples.append(([nltk.tree.Tree.fromstring('('+re.sub(r"([^()\s]+)", r"(W \1)", s)+')') for s in [s1,s2]],relation))
-  random.shuffle(examples)
-  nTest = len(examples)//5
-  trainData = examples[:4*nTest]
-  testData = examples[4*nTest:]
-  trialData = []
-
-  with open(os.path.join('data',name+'.pik'), 'wb') as f:
-    pickle.dump([trainData, testData, trialData, vocabulary], f, -1)
-
-def sickData(name):
-  source = os.path.join('data',name)
-  # make a list of files to open
-  if os.path.isdir(source):
-    toOpen = [os.path.join(source,f) for f in os.listdir(source)]
-    toOpen = [f for f in toOpen if os.path.isfile(f)]
-  elif os.path.isfile(source): toOpen = [source]
+  else: 
+    print 'cannot open source', source
+    sys.exit()
   examples = {'TRAIN':[],'TEST':[],'TRIAL':[]}
-  vocabulary = ['UNK']
+  vocabulary = set()
   for f in toOpen:
     with open(f,'r') as f:
-      next(f) # skip header
+      if 'sick' in name: f.next()
       for line in f:
-        bits = line.split('\t')
-        s1 = bits[2]
-        s2 = bits[4]
-        relation = bits[5]
-        kind = bits[-1].strip()
-        for word in s1.split()+s2.split():
-          if word !=')' and word != '(' and word not in vocabulary:
-            vocabulary.append(word)
-          # add training example to set
-        trees = [nltk.tree.Tree.fromstring('('+re.sub(r"([^()\s]+)", r"(W \1)", s)+')') for s in [s1,s2]]
-        [nltk.treetransforms.chomsky_normal_form(t) for t in trees]
-        [nltk.treetransforms.collapse_unary(t, collapsePOS = True,collapseRoot = True) for t in trees]
+        if random.randint(0,4)<1: kind = 'TEST'
+        else: kind = 'TRAIN'
+        if 'sick' in name:
+          bits = line.split('\t')
+          ts = [nltk.tree.Tree.fromstring(s) for s in [bits[2],bits[4]]]
+          relation = bits[5]
+          kind = bits[-1].strip()
+        elif 'bowman' in name:
+          relation, s1, s2  = line.split('\t')
+          ts = [nltk.tree.Tree.fromstring('('+re.sub(r"([^()\s]+)", r"(W \1)", s)+')') for s in [s1,s2]]
+        else:
+          ts = [nltk.tree.Tree.fromstring(line)]
+          relation = None
+        [nltk.treetransforms.chomsky_normal_form(t) for t in ts]
+        [nltk.treetransforms.collapse_unary(t, collapsePOS = True,collapseRoot = True) for t in ts]
+        if kind == 'TRAIN': vocabulary.update([w.lower() for w in t.leaves() for t in ts])
+        if relation: examples[kind].append(tuple([ts,relation]))
+        else: examples[kind].append(ts)
+  vocabulary = list(vocabulary)
+  vocabulary.insert(0, 'UNK')
+  print len(vocabulary),'words,', len(examples['TRAIN']),'training examples,', len(examples['TEST']),'test examples.'
 
-        examples[kind].append((trees,relation))
   with open(os.path.join('data',name+'.pik'), 'wb') as f:
-    pickle.dump([examples['TRAIN'],examples['TEST'],examples['TRIAL'],vocabulary], f, -1)
+    pickle.dump([examples,vocabulary], f, -1)
 
 
-# name = 'bowman14'
-# artData(name)
-# 
-# name = 'bowman15'
-# artData(name)
-
-# name = 'sick'
-# sickData(name)
-
-name = 'senna'
-getSennaEmbs(name)
+#name = 'senna'
+#getSennaEmbs(name)
 # with open('data/sennaV.pik', 'wb') as f:
 #   V, voc =   pickle.load(f)
-#
-with open(os.path.join('data',name+'.pik'),'rb') as f:
-  data = pickle.load(f)
+
+
+names = ['bowman14','bowman15','sick','sickSample','flickr']
+for name in names:
+  print name
+  storeTrees(name)
+
+# with open(os.path.join('data',name+'.pik'),'rb') as f:
+#   data = pickle.load(f)
 
