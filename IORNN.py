gradient check was wrong, so not optimizing objective. Looks better now, but composition nodes are not good yet

IORNN.py
@@ -1,6 +1,6 @@
 from __future__ import division
-import nltk
-from nltk.tree import Tree
+#import nltk
+#from nltk.tree import Tree
 import numpy as np
 import sys
 from collections import defaultdict
@@ -41,7 +41,7 @@ class Node:
     self.sibling = sibling
 
   def backpropInner(self,delta,theta,gradients):
-#    print 'backpropInner', self
+#    print 'node.backpropInner', self, np.shape(delta), delta
     childrenas = np.concatenate([child.innerA for child in self.children])
     childrenads = np.concatenate([child.innerAd for child in self.children])
     # compute delta to backprop and backprop it
@@ -52,7 +52,10 @@ class Node:
     gradients[self.cat+'IB']+=delta
 
   def backpropOuter(self, delta, theta, gradients):
-#    print 'backpropOuter', self#, self.sibling
+    print 'backpropOuter', self.cat, self#, self.sibling
+    # input: OUTER of the parent
+    #        INNER of the sibling (if it exists)
+
     if self.parent:
       As = self.parent.outerA
       Ads = self.parent.outerAd
@@ -64,15 +67,20 @@ class Node:
 #      print cat, np.shape(M), np.shape(delta)
       deltaB = np.multiply(np.transpose(M).dot(delta), Ads)
       if self.sibling:
-#        print 'backproping to parent and sibling'
+        print '\t backproping to parent and sibling'
         deltaB = np.split(deltaB,2)
         self.parent.backpropOuter(deltaB[0], theta, gradients)
         self.sibling.backpropInner(deltaB[1], theta, gradients)
-      else: self.parent.backpropOuter(deltaB, theta, gradients)
+      else:
+        print '\t backproping to parent only'
+        self.parent.backpropOuter(deltaB, theta, gradients)
       gradients[cat+'OM']+= np.outer(delta,As)
       gradients[cat+'OB']+= delta
+#    else: print 'backpropouter stops: top node'
+
 
   def inner(self, theta):
+#    print 'node.inner', self.cat
 #    inputsignal = np.concatenate([child.inner(theta) for child in self.children])
     inputs = [child.inner(theta) for child in self.children]
 
@@ -88,6 +96,7 @@ class Node:
     return self.innerA
 
   def outer(self, theta):
+#    print 'node.outer', self.cat
 #    print 'outer called for:', self,  'of cat', self.cat
     if not self.parent:
       self.outerZ = np.zeros_like(theta[self.cat+'IB'])
@@ -104,24 +113,27 @@ class Node:
 
   def train(self, theta, gradients = None, target = None):
 #    if gradients is None: gradients = np.zeros_like(theta)
-#    print 'start training'
+    print 'network',self,'start training with target', target
     if gradients is None:
-      gradients = theta.zeros_like() #sparse.csc_from_dense(np.zeros_like(theta))
-#      print 'created sparse matrix'
+      gradients = theta.zeros_like()
     self.activateNW(theta)
     error = np.mean([leaf.trainWords(theta, gradients,target) for leaf in self.leaves()])
-#    [child.train(theta, None, gradients) for child in self.children]
     if error>2: print 'nw error:', error
 
     return gradients,error
 
-  def score(self,theta, x=15):
-    summedC = 0
-    for leaf in self.leaves():
-      scorew = leaf.score(theta, -1, False)[0]
-      scorex = leaf.score(theta, x, False)[0]
-      summedC += max(0,1 - scorew+scorex)
-    return summedC
+  def error(self,theta, x=0, recompute = False):
+#    print 'nw.error called for', x
+#    if not type(x) is int: print 'nw.score called for', x
+#    return sum([leaf.score(theta,x,recompute) for leaf in self.leaves()])
+     summedC = 0
+     for leaf in self.leaves():
+       scorew = leaf.score(theta, -1, recompute)[0]
+       scorex = leaf.score(theta, x, recompute)[0]
+       c= max(0,1 - scorew+scorex)
+#  #     print 'c:',c
+       summedC += c
+     return summedC
 
   def predict(self, theta):
     return max([c.predict(theta) for c in self.children])
@@ -135,19 +147,53 @@ class Node:
     elif self.cat == 'score': return 's'
     else: return '('+' '.join([str(child) for child in self.children])+')'
 
+  def numericalGradient(self, theta, target = None):
+    nw = self
+    nw.activateNW(theta)
+    print 'Computing numerical gradient for target', target
+  #  print 'numgrad', theta.dtype.names
+    epsilon = 0.001
+    numgrad = theta.zeros_like(False)
+#    score0 = nw.score(theta, target, True)
+    error0 = nw.error(theta,target,True)
+#    print 'score0:', score0
+#    print 'error:', error0
+    for name in theta.keys():
+        print '\t',name
+    # create an iterator to iterate over the array, no matter its shape
+        it = np.nditer(theta[name], flags=['multi_index'])
+        while not it.finished:
+          i = it.multi_index
+          old = theta[name][i]
+          theta[name][i] = old + epsilon
+ #         errorPlus = max(0,1-score0+nw.score(theta,target))
+          errorPlus=nw.error(theta,target,True)
+          theta[name][i] = old - epsilon
+#          errorMin = max(0,1-score0+nw.score(theta,target))
+          errorMin=nw.error(theta,target,True)
+          d =(errorPlus-errorMin)/(2*epsilon)
+#          if d!=0: print '\t\tchange gradient, diff:', errorPlus-errorMin
+          numgrad[name][i] = d
+          theta[name][i] = old  # restore theta
+          it.iternext()
+    return numgrad
+
+
 class Leaf(Node):
   def __init__(self, cat, index, actO, word=''):
     children = [Node([Node([], 'score', 'identity','identity')], 'u', 'tanh','tanh')]
+#    children = [Node([Node([], 'score', 'sigmoid','sigmoid')], 'u', 'tanh','tanh')]
     Node.__init__(self,children,cat,'identity',actO)
     children[0].setRelatives(self,self)
+    children[0].children[0].setRelatives(children[0],None)
     # this node should behave as a sibling (produce an inner representation) and a parent (outer) to the u node
     self.index = index
     self.word = word
 
   def trainWords(self, theta, gradients, target = None):
-
+    print 'trainWords', target
     nwords = len(theta[self.cat+'IM'])
-    scorew = self.score(theta, False)[0]
+    scorew = self.score(theta, recompute= False)
     # pick a candidate x different from own index
 
     if target is None:
@@ -155,11 +201,32 @@ class Leaf(Node):
       while x == self.index:  x = random.randint(0,nwords-1)
     else: x = target
     # if the candidate scores too high: backpropagate error
-    scorex = self.score(theta, x, False)[0]
-    c = max(0,1 - scorew+scorex)
-    if c>1:
+    scorex = self.score(theta, x, False)
+    c = 1 - scorew+scorex
+    #c = max(0,1 - scorew+scorex)
+#    if c<0:
+#      print 'FYI: c smaller than zero!'
+#      c = 0*c
+#    print 'c:', c
+
+    if True: #c>1:
+      delta = np.array([-1])
+
+      self.children[0].children[0].backpropOuter(delta, theta, gradients)
+      original = self.index
+      self.index = target
+      self.recomputeNW(theta)
+      #self.inner(theta)
+      #self.outer(theta)
       delta = np.array([1])
       self.children[0].children[0].backpropOuter(delta, theta, gradients)
+      self.index = original
+      self.recomputeNW(theta) #self.inner(theta)
+#      self.outer(theta)
+
+
+ #   self.children[0].children[0].backpropOuter(1-c, theta, gradients)
+
 #    return gradients
     if c>2: print 'leaf c:', c, self.word
     return c
@@ -168,19 +235,22 @@ class Leaf(Node):
     return [self]
 
   def score(self, theta, wordIndex=-1, recompute = True):
+    recompute = True
     if recompute: self.recomputeNW(theta)
     # pretend the index is the candidate
-    if wordIndex > 0:
+    if wordIndex > -1:
       trueIndex = self.index
       self.index = wordIndex
-      self.inner(theta)
-      self.outer(theta)
+      self.recomputeNW(theta)
+#      self.inner(theta)
+#      self.outer(theta)
     score = self.children[0].children[0].outerA
     # reset everything
-    if wordIndex > 0:
+    if wordIndex > -1:
       self.index = trueIndex
-      self.inner(theta)
-      self.outer(theta)
+      self.recomputeNW(theta)
+#      self.inner(theta)
+#      self.outer(theta)
     return score[0]
 
   def predict(self, theta):
@@ -188,12 +258,13 @@ class Leaf(Node):
       scores = []
       for index in xrange(len(theta['relIM'])):
 #        print index
-        scores.append(self.score(theta,index))
+        scores.append(self.score(theta,index, False))
       return scores.index(max(scores))
     else: return None
 
 
   def inner(self, theta):
+#    print 'leaf.inner', self.cat
     if self.cat == 'rel': print 'Leaf.inner', self.cat, self.index
     self.innerZ = np.asarray(theta[self.cat+'IM'][self.index]).flatten()
     # after theta is updated, the wordIM has become a matrix instead of a 2D-array.
@@ -203,19 +274,13 @@ class Leaf(Node):
     return self.innerA
 
   def backpropInner(self,delta, theta, gradients):
-#    gradients[self.cat+'IM'] += delta
-
+#    print 'leaf.backpropInner', self, np.shape(delta), delta
     d = len(delta)
     row = np.array([self.index]*d)
     col = np.arange(d)
 #    print delta.shape, d
     deltaM = sparse.csc_matrix((delta,(row,col)),shape=np.shape(theta[self.cat+'IM']))
     gradients[self.cat+'IM'] = gradients[self.cat+'IM']+(deltaM)
-#    print 'backpropInner', self
-#     d = len(theta[self.cat+'IM'][0])
-#     deltaM = sparse.csc_matrix((delta,np.arange(d),[self.index]*d),shape=np.shape(theta[self.cat+'IM']))
-#     sparse.basic.add(gradients[self.cat+'IM'], deltaM)
-#        sparse.basic.add(gradients[self.cat+'IM'][self.index,:], delta)
 
   def __str__(self):
     return self.word
@@ -224,25 +289,3 @@ class Leaf(Node):
 
 
 
-def numericalGradient(theta, nw, target = None):
-  print 'Computing numerical gradient.'
-#  print 'numgrad', theta.dtype.names
-  epsilon = 0.0001
-  numgrad = theta.zeros_like(False)
-  score0 = nw.score(theta)
-  for name in theta.keys():
-    print '\t',name
-  # create an iterator to iterate over the array, no matter its shape
-      it = np.nditer(theta[name], flags=['multi_index'])
-      while not it.finished:
-        i = it.multi_index
-        old = theta[name][i]
-        theta[name][i] = old + epsilon
-        errorPlus = max(0,1-score0+nw.score(theta))
-        theta[name][i] = old - epsilon
-        errorMin = max(0,1-score0+nw.score(theta))
-        d =(errorPlus-errorMin)/(2*epsilon)
-        numgrad[name][i] = d
-        theta[name][i] = old  # restore theta
-        it.iternext()
-  return numgrad

gradientCheck.py
@@ -1,24 +1,60 @@
+import naturalLogicCopy as nl
+from nltk import tree
+import myTheta
+import numpy as np
+from scipy import sparse
+
 def gradientCheck(theta, network, target):
   network.activateNW(theta)
 
   # compute analyticial and numerical gradient
-  grad = network.backprop(theta, target)
+  #grad = network.backprop(theta, target)
+  print 'computing analytical gradient'
+  grad, err = network.train(theta, None, target)
+
+#   if err<=1:
+#     print 'no backpropagation.'
+#     return
+  print 'computing numerical gradient'
   numgrad = network.numericalGradient(theta,target)
 
+
+
+
+  print 'comparing'
   # flatten gradient objects and report difference
   gradflat = np.array([])
   numgradflat = np.array([])
-  for name in theta.dtype.names:
+  for name in theta.keys():
     ngr = np.reshape(numgrad[name],-1)
+    if sparse.issparse(grad[name]): grad[name] = grad[name].todense()
     gr = np.reshape(grad[name],-1)
     if np.array_equal(gr,ngr): diff = 0
     else: diff = np.linalg.norm(ngr-gr)/(np.linalg.norm(ngr)+np.linalg.norm(gr))
     print 'Difference '+name+' :', diff
+    if False:#'composition'in name: #diff>0:
+      print 'gr\t\tngr\t\td'
+      for i in range(len(gr)):
+        print str(gr[i])+'\t'+str(ngr[i])+'\t'+str(gr[i]-ngr[i])
     gradflat = np.append(gradflat,gr)
     numgradflat = np.append(numgradflat,ngr)
   print 'Difference overall:', np.linalg.norm(numgradflat-gradflat)/(np.linalg.norm(numgradflat)+np.linalg.norm(gradflat))
 
+def checkIORNN():
+  voc = ['UNK','most','large', 'hippos','bark','chase','dogs']
+#  voc = ['UNK','most','hippos']
+  d = 3
+  dims = {'inside':d,'outside':d,'word':d,'nwords':len(voc)}
+  theta = myTheta.Theta('IORNN', dims)
 
+#  s = '(S (NP (Q most) (N hippos)) (VP (V chase) (NP (A big) (N dogs))))'
+#  s = '(S (NP (Q most) (N (A big) (N hippos))) (VP (V chase) (NP (A big) (N dogs))))'
+  s = '(NP (Q most) (N hippos))'
+#  s = '(Q most)'
+  t = tree.Tree.fromstring(s)
+  nw = nl.iornnFromTree(t, voc, grammarBased = False)
+  nw.activateNW(theta)
+  gradientCheck(theta, nw, 0)
 
-
+checkIORNN()
 